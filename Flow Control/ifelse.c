1baa9aaea72207789f6779a9c29012c5e6788511
Niranjan Powade <niranjanpowadex@gmail.com>
2023-09-06 23:50:17 +0530

    Learning about Flow Control in C

---
+++ Flow Control/ifelse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 20;
+
+    if (age >= 18)
+    {
+        printf("You are an adult.\n");
+    }
+    else
+    {
+        printf("You are not an adult.\n");
+    }
+
+    return 0;
+}
