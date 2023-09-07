a8fa37e628aa8f2bec56c5c51e298f7e89487c81
Niranjan Powade <niranjanpowadex@gmail.com>
2023-09-07 23:06:54 +0530

    Learning about Arrays in C

---
+++ Arrays/ThreeDimensionalArrays.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int cube[2][3][4] = {
+        {{1, 2, 3, 4},
+         {5, 6, 7, 8},
+         {9, 10, 11, 12}},
+        {{13, 14, 15, 16},
+         {17, 18, 19, 20},
+         {21, 22, 23, 24}}};
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                printf("Element at (%d, %d, %d): %d\n", i, j, k, cube[i][j][k]);
+            }
+        }
+    }
+
+    return 0;
+}
