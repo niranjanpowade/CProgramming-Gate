9e3f22bbf030585a23d5ac50e0a60aa8337e78d9
Niranjan Powade <niranjanpowadex@gmail.com>
2023-09-23 16:06:43 +0530

    One array element was missing

--- Pointers in C/Problems/Question3.c
+++ Pointers in C/Problems/Question3.c
@@ -3,13 +3,12 @@
 int main()
 {
     int a[] = {10, 20, 30, 40, 50, 60};
-    int *p[] = {a, a + 1, a + 3, a + 4, a + 5};
+    int *p[] = {a, a + 1, a + 2, a + 3, a + 4, a + 5};
     int **pp = p;
     pp++;
-    ;
     printf("%d,%d,%d\n", pp - p, *pp - a, **pp); // 1,1,20
     *pp++;
-    printf("%d,%d,%d\n", pp - p, *pp - a, **pp); // 2,3,40
+    printf("%d,%d,%d\n", pp - p, *pp - a, **pp); // 2,2,30
     ++*pp;
-    printf("%d,%d,%d\n", pp - p, *pp - a, **pp); // 2,4,50
+    printf("%d,%d,%d\n", pp - p, *pp - a, **pp); // 2,3,40
 }
